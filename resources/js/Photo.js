df7273b00ba6d3092e46ef339a3ddf741d518a4b
UncleKo <info@shrewd.work>
2019-07-14 02:19:24 +0900

    Dropzone

--- resources/js/Photo.js
+++ resources/js/Photo.js
@@ -1,5 +1,8 @@
-import React from 'react';
+// import React from 'react';
+import React from 'react'
 import axios from 'axios';
+
+import Dropzone from './Dropzone';
 
 class PhotoList extends React.Component {
 
@@ -26,24 +29,36 @@ class PhotoList extends React.Component {
     this.getData();
   }
 
-  addPhoto = (newPhoto) => {
-    // console.log(newPhoto)
-    this.setState(state => ({
-      photos: [newPhoto, ...state.photos]
-    }));
+  // addPhoto = (newPhoto) => {
+  //   // console.log(newPhoto)
+  //   this.setState(state => ({
+  //     photos: [newPhoto, ...state.photos]
+  //   }));
+  // }
+
+  insertImage = (data) => {
+    // console.log(e.target.dataset.medium)
+    let image = `<a href="${data.origin}"><img src="${data.medium}"></a>`;
+    document.querySelector('#id_content').value += image;
   }
 
   postData = (e) => {
     e.preventDefault();
     // this.fileUpload(this.state.file).then(this.getData());
     this.fileUpload(this.state.file).then( res => {
-      this.addPhoto(res.data);
+      // this.addPhoto(res.data);
+      this.insertImage(res.data);
     });
   }
 
-  onChange = (e) => {
-    // console.log(e.target.files[0]);
-    this.setState({file:e.target.files[0]});
+  // onChange = (e) => {
+  //   // console.log(e.target.files[0]);
+  //   this.setState({file:e.target.files[0]});
+  // }
+
+  dropChange = (file) => {
+    // console.log(file);
+    this.setState({file: file})
   }
 
   fileUpload = (file) => {
@@ -62,10 +77,20 @@ class PhotoList extends React.Component {
   //   modal.classList.add('show-modal');
   // }
 
-  insertImage = (e) => {
+  insertImageFromList = (e) => {
     // console.log(e.target.dataset.medium)
     let image = `<a href="${e.target.dataset.origin}"><img src="${e.target.dataset.medium}"></a>`;
     document.querySelector('#id_content').value += image;
+  }
+
+  showList = () => {
+    document.querySelector('.photo-upload').style.display = 'none';
+    document.querySelector('.photo-list').style.display = 'block';
+  }
+
+  hideList = () => {
+    document.querySelector('.photo-upload').style.display = 'block';
+    document.querySelector('.photo-list').style.display = 'none';
   }
 
   render() {
@@ -73,26 +98,31 @@ class PhotoList extends React.Component {
     let elements = this.state.photos.map((photo,index) => {
       return (
       <li key={photo.id}>
-        <img className="thumb" onClick={this.insertImage} src={ photo.thumbnail } data-medium={ photo.medium } data-origin={ photo.origin }/> 
+        <img className="thumb" onClick={this.insertImageFromList} src={ photo.thumbnail } data-medium={ photo.medium } data-origin={ photo.origin }/> 
       </li>
       )
     })
     return (
       <div>
+        <div className="photo-upload">
+          <Dropzone dropChange={this.dropChange} />
+          <button className="btn btn-primary" onClick={this.postData}>アップロードして画像を挿入</button>
+          <p>or</p>
+          <button className="btn btn-outline-info keep-modal" onClick={this.showList}>アルバムから選択して挿入</button>
+        </div>
         <div className="photo-list">
-          <small>クリックして画像を挿入</small>
+          <small>画像をクリックして挿入</small>
+          <button className="btn btn-outline-info keep-modal" onClick={this.hideList}>写真をアップロードする</button>
           <ul>
             {elements}
           </ul>
         </div>
-        <small>or 画像をアップロード</small>
-        
         {/* <input type="file" className="keep-modal" onChange={this.onChange} />
         <button className="btn btn-primary keep-modal" onClick={this.postData}>アップロード</button> */}
-        <form method="POST" action="http://127.0.0.1:8000/api/photos/" onSubmit={this.postData}>
+        {/* <form method="POST" action="http://127.0.0.1:8000/api/photos/" onSubmit={this.postData}>
           <input type="file" className="keep-modal" onChange={this.onChange} />
           <button className="btn btn-primary keep-modal" type="submit">アップロード</button>
-        </form>
+        </form> */}
       </div>
     )
   } //render
